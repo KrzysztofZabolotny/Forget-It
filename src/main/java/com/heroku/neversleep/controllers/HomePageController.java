57ab9989f70bdcebc006c9aa08088e42a2168593
Krzysztof Zabolotny <s19073@pjwstk.edu.pl>
2021-07-22 23:07:02 +0200

    Added ExecutorService to sendRequestMethod

---
+++ src/main/java/com/heroku/neversleep/controllers/HomePageController.java
@@ -0,0 +1,19 @@
+package com.heroku.neversleep.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+
+
+    @GetMapping("/")
+    public String home(){
+
+
+        return "index";
+    }
+
+
+}
